a8101acba32a055dc6286202f81df8277fcdf018
David Urdaibay <urdaibad@tcd.ie>
2019-10-11 22:00:53 +0100

    Added new test for insert function.

--- LCA/src/LCAtests.java
+++ LCA/src/LCAtests.java
@@ -1,6 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
 
 class LCAtests {
 
@@ -15,10 +15,26 @@ class LCAtests {
 	 * 												     5
 	 * 												    /
 	 * 												   4												    
-	 */															
-	
+	 */	
 	@Test
-	void findLCATest() {				
+	public void testInsert() {
+		BST bst = new BST();
+		bst.insert(8);					
+		bst.insert(6);
+		bst.insert(7);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(9);
+		bst.insert(10);
+		bst.preOrderTraversal();
+	}
+	
+	@org.junit.Ignore
+	@Test
+	public void testLCA() {				
 		BST bst = new BST();		
 		bst.insert(8);					
 		bst.insert(6);
@@ -30,6 +46,7 @@ class LCAtests {
 		bst.insert(4);
 		bst.insert(9);
 		bst.insert(10);
+		System.out.println("hello");
 		Node lca = bst.findLCA(1, 4);
 		assertEquals(2, lca.data);
 	}
