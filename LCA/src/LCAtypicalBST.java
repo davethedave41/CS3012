6d927f75bee5c88ef91c4f6014c9c154cc55df54
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 13:24:22 +0100

    Added tests to typicalBST test case.

--- LCA/src/LCAtypicalBST.java
+++ LCA/src/LCAtypicalBST.java
@@ -3,10 +3,68 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class LCAtypicalBST {
-
+	
+	/* Tree:                                             8
+	 * 												    / \
+	 * 												   6   9
+	 * 												  / \	\	
+	 * 												 2   7  10
+	 * 												/ \      
+	 *                                             1   3
+	 * 												    \
+	 * 												     5
+	 * 												    /
+	 * 												   4												    
+	 */	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTypicalBST1() {
+		BST bst = new BST();
+		bst.insert(8);					
+		bst.insert(6);
+		bst.insert(7);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(9);
+		bst.insert(10);
+		Node lca = bst.findLCA(10, 5);
+		assertEquals(8, lca.data, "Should be 8");
 	}
 
+	@Test
+	void testTypicalBST2() {
+		BST bst = new BST();
+		bst.insert(8);					
+		bst.insert(6);
+		bst.insert(7);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(9);
+		bst.insert(10);
+		Node lca = bst.findLCA(1, 4);
+		assertEquals(2, lca.data, "Should be 2");
+	}
+	
+	@Test
+	void testTypicalBST3() {
+		BST bst = new BST();
+		bst.insert(8);					
+		bst.insert(6);
+		bst.insert(7);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(9);
+		bst.insert(10);
+		Node lca = bst.findLCA(7, 3);
+		assertEquals(6, lca.data, "Should be 6");
+	}
 }
