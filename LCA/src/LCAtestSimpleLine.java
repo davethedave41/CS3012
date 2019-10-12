2320702111936e6fe30b5410f2843e48606a67c5
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 12:24:08 +0100

    Added graph representation.

--- LCA/src/LCAtestSimpleLine.java
+++ LCA/src/LCAtestSimpleLine.java
@@ -1,48 +1,23 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;	
-
 
 class LCAtestSimpleLine{
-
-	private String outputPrint;
-	ConsoleToString cP = new ConsoleToString();
-
-	/* Tree:                     						           		  8
-	 * 												    / \
-	 * 												   6   9
-	 * 												  / \	\	
-	 * 												 2   7  10
-	 * 												/ \      
-	 * 		                                     					       1   3
-	 * 												    \
-	 * 												     5
-	 * 												    /
-	 * 												   4												    
-	 */	
-	@Disabled("Should work because they are the same...")
-	@Test
-	public void testInsert() {
-		BST bst = new BST();
-		bst.insert(8);					
-		bst.insert(6);
-		bst.insert(7);
-		bst.insert(2);
-		bst.insert(3);
-		bst.insert(5);
-		bst.insert(1);
-		bst.insert(4);
-		bst.insert(9);
-		bst.insert(10);
-		outputPrint = cP.consoleText(bst);
-		assertEquals("8 6 2 1 3 5 4 7 9 1 0", outputPrint);
-	}
+	
 	/* Tree: 
-	 * 
-	 * 
+	 *                    1
+	 *                     \
+	 *                      2
+	 *                       \
+	 *                        8
+	 *                         \
+	 *                          10
+	 *                           \
+	 *                            11
+	 *                             \
+	 *                              12
 	 */
 
-	
+	@Test
 	public void testSimpleLineForm() {				
 		BST bst = new BST();		
 		bst.insert(1);					
@@ -52,8 +27,6 @@ class LCAtestSimpleLine{
 		bst.insert(11);
 		bst.insert(12); 
 		Node lca = bst.findLCA(11, 12);
-		assertEquals(11, lca.data, "Expected LCA is 11");
-		lca = bst.findLCA(8, 10);
-		assertEquals(8, lca.data, "Expected LCA is 8");
+		assertEquals(10, lca.data, "Expected LCA is 11");
 	}
 }
