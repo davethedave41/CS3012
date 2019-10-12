d0fc87a71b6b12fbc042f7f3a3d87303b83d3198
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 12:35:19 +0100

    Added left leaning and right leaning simple line BST tests.

--- LCA/src/LCAtestSimpleLine.java
+++ LCA/src/LCAtestSimpleLine.java
@@ -18,7 +18,7 @@ class LCAtestSimpleLine{
 	 */
 
 	@Test
-	public void testSimpleLineForm() {				
+	public void testRightLeaning() {				
 		BST bst = new BST();		
 		bst.insert(1);					
 		bst.insert(2);
@@ -27,6 +27,32 @@ class LCAtestSimpleLine{
 		bst.insert(11);
 		bst.insert(12); 
 		Node lca = bst.findLCA(11, 12);
-		assertEquals(10, lca.data, "Expected LCA is 11");
+		assertEquals(11, lca.data, "Expected LCA is 11");
+	}
+	/* Tree:  
+	 *                                12  
+	 *                                /
+	 *                              11
+	 *                              /
+	 *                            10
+	 *                            /
+	 *                           8
+	 *                          /
+	 *                         2
+	 *                        /
+	 *                       1   
+	 */
+
+	@Test
+	public void testLeftLeaning() {				
+		BST bst = new BST();		
+		bst.insert(12);					
+		bst.insert(11);
+		bst.insert(10);
+		bst.insert(8);
+		bst.insert(2);
+		bst.insert(1); 
+		Node lca = bst.findLCA(1, 8);
+		assertEquals(8, lca.data, "Expected LCA is 8");
 	}
 }
