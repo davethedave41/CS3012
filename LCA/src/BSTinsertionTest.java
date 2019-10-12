ef887803f68e2fd772ac6b04d9c859257173f54c
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 10:54:22 +0100

    New test case for testing BST insert function.

---
+++ LCA/src/BSTinsertionTest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class BSTinsertionTest {
+	
+	private String outputPrint;
+	ConsoleToString cP = new ConsoleToString();
+	
+	/* Tree:                     						           		  8
+	 * 												    / \
+	 * 												   6   9
+	 * 												  / \	\	
+	 * 												 2   7  10
+	 * 												/ \      
+	 * 		                                     					       1   3
+	 * 												    \
+	 * 												     5
+	 * 												    /
+	 * 												   4												    
+	 */	
+
+	@Disabled("Should work because they are the same...")
+	@Test
+	public void testInsert() {
+		BST bst = new BST();
+		bst.insert(8);					
+		bst.insert(6);
+		bst.insert(7);
+		bst.insert(2);
+		bst.insert(3);
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(9);
+		bst.insert(10);
+		outputPrint = cP.consoleText(bst);
+		assertEquals("8 6 2 1 3 5 4 7 9 1 0", outputPrint);
+	}
+}
