2e9e3d44b2136ae6db8b89d57c1728569243ff60
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 10:55:25 +0100

    Added a test suite to have seperate test cases for each type of BST graphs.

---
+++ LCA/src/LCAbstTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({LCAtestSimpleLine.class, BSTinsertionTest.class, LCAtestOneNode.class, LCAtestEmpty.class, LCAtypicalBST.class})
+public class LCAbstTests {
+
+}
