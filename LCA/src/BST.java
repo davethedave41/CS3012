4a24acffe406d5a50031db0a8bb803dadf8456b1
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 13:40:42 +0100

    Deleted blank lines and added a case for a null BST in findLCA function.

--- LCA/src/BST.java
+++ LCA/src/BST.java
@@ -14,7 +14,7 @@ public class BST
 {  
 	Node root; 
 	static boolean v1 = false, v2 = false; 
-	
+
 	public void insert(int val){
 		if (root == null) { 
 			root = new Node(val);
@@ -24,7 +24,7 @@ public class BST
 		root = insert(root, val);
 		//System.out.println("Insertion of val: "+val+" complete");
 	}
-	
+
 	private Node insert(Node x,int val) {
 		if (x == null) return new Node(val);	
 		if      (val < x.data) { x.left  = insert(x.left,val); /*System.out.println("went left");*/}  
@@ -32,11 +32,11 @@ public class BST
 		//else     System.out.println("already in tree");
 		return x;
 	}
-	
+
 	public void preOrderTraversal() {
 		preOrderTraversal(root);
 	}
-	
+
 	private Node preOrderTraversal(Node x) {
 		if(x == null) return null;
 		System.out.print(x.data+" ");
@@ -44,16 +44,12 @@ public class BST
 		preOrderTraversal(x.right);
 		return x;
 	}
-
+	
 	Node findLCAUtil(Node node, int n1, int n2) 
 	{ 
-
 		if (node == null) 
 			return null; 
-	
-		
 		Node temp=null; 
-
 		if (node.data == n1) 
 		{ 
 			v1 = true; 
@@ -64,31 +60,26 @@ public class BST
 			v2 = true; 
 			temp = node; 
 		} 
- 
 		Node left_lca = findLCAUtil(node.left, n1, n2); 
 		Node right_lca = findLCAUtil(node.right, n1, n2); 
-
 		if (temp != null) 
 			return temp; 
-
 		if (left_lca != null && right_lca != null) 
 			return node; 
-
 		return (left_lca != null) ? left_lca : right_lca; 
 	} 
 
-	
+
 	Node findLCA(int n1, int n2) 
 	{ 
 
 		v1 = false; 
 		v2 = false; 
-
+		if(root == null) 
+			return null;
 		Node lca = findLCAUtil(root, n1, n2); 
-
 		if (v1 && v2) 
 			return lca; 
-
 		return null; 
 	} 
 } 
