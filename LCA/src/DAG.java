d95378e3002c7fdc85fe22d759330a3576ff3ca5
David Urdaibay <urdaibad@tcd.ie>
2019-10-12 14:59:26 +0100

    Starting DAG implementation.

---
+++ LCA/src/DAG.java
@@ -0,0 +1,15 @@
+
+public class DAG {
+	
+	private int adjacencyMatrix[][];
+	private String vertices[];
+	
+	DAG(int adjacencyMatrix[][],int edges[],String vertices[]){
+		this.adjacencyMatrix = adjacencyMatrix;
+		this.vertices = vertices;
+	}
+	
+	public void addEdge() {
+		
+	}
+}
